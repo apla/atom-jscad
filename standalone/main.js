95d0c6fad95811238289065b087fb4c956b8ead1
Matias Insaurralde <matias@insaurral.de>
2015-11-02 02:54:56 -0300

    using window.jscadscript

--- standalone/main.js
+++ standalone/main.js
@@ -1,12 +1,13 @@
-  var gCurrentFile,
-      gProcessor;
+var gCurrentFile,
+gProcessor;
 
-  OpenJsCad.AlertUserOfUncaughtExceptions();
+OpenJsCad.AlertUserOfUncaughtExceptions();
 
-  gProcessor = new OpenJsCad.Processor(document.getElementById("viewer"));
-  // gProcessor.setDebugging( true );
+gProcessor = new OpenJsCad.Processor(document.getElementById("viewer"));
+// gProcessor.setDebugging( true );
 
-  var jscadscript = 'function main() { var cube = CSG.roundedCube({radius: 10, roundradius: 2, resolution: 16}); var sphere = CSG.sphere({radius: 10, resolution: 16}).translate([5, 5, 5]); return cube.union(sphere); }',
-      filename = 'test.jscad';
-
-  gProcessor.setJsCad( jscadscript, filename );
+/*
+var jscadscript = 'function main() { var cube = CSG.roundedCube({radius: 10, roundradius: 2, resolution: 16}); var sphere = CSG.sphere({radius: 10, resolution: 16}).translate([5, 5, 5]); return cube.union(sphere); }',
+filename = 'test.jscad';
+*/
+gProcessor.setJsCad( jscadscript, 'test.jscad' );
