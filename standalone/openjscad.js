f0846776ac019f0f8cf6a1e6f6146c83353cb3f2
apla <dot.and.thing@gmail.com>
2016-06-12 14:25:58 +0300

    global variables cleanup; using bundle load

--- standalone/openjscad.js
+++ standalone/openjscad.js
@@ -473,7 +473,7 @@ OpenJsCad.parseJsCadScriptSync = function(script, mainParameters, debugging) {
 // callback: should be function(error, csg)
 OpenJsCad.parseJsCadScriptASync = function(script, mainParameters, options, callback) {
 
-  var baselibraries = [ jsDependencies[5], jsDependencies[7]  ];
+  var baselibraries = OpenJsCad.workerDeps || [ jsDependencies[5], jsDependencies[7]  ];
   console.log( 'baselibraries', baselibraries );
   var baseurl = document.location.href.replace(/\?.*$/, '');
   // var baseurl = 'file:///Users/matias/.atom/packages/atom-scad-preview';
