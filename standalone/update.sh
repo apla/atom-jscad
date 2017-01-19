b547a38a26db2214f53cc2158e394ceb3ec5e27f
John Cole <johnwebbcole@gmail.com>
2017-01-18 22:46:08 -0600

    no message

--- standalone/update.sh
+++ standalone/update.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 
 set -e
+[ "$1" == "--overwrite" ] && {
+  OVERWRITE=$1
+}
 
 mkdir -p openjscad/imgs
 
-curl https://raw.githubusercontent.com/Spiritdude/OpenJSCAD.org/master/openjscad.js -o openjscad/openjscad.js
-curl https://raw.githubusercontent.com/Spiritdude/OpenJSCAD.org/master/openscad.js -o openjscad/openscad.js
-curl https://raw.githubusercontent.com/Spiritdude/OpenJSCAD.org/master/js/jscad-function.js -o openjscad/jscad-function.js
-curl https://raw.githubusercontent.com/Spiritdude/OpenJSCAD.org/master/js/jscad-worker.js -o openjscad/jscad-worker.js
-curl https://raw.githubusercontent.com/Spiritdude/OpenJSCAD.org/master/lightgl.js -o openjscad/lightgl.js
-curl https://raw.githubusercontent.com/Spiritdude/OpenJSCAD.org/master/csg.js -o openjscad/csg.js
-curl https://raw.githubusercontent.com/Spiritdude/OpenJSCAD.org/master/Blob.js -o openjscad/Blob.js
-curl https://raw.githubusercontent.com/Spiritdude/OpenJSCAD.org/master/imgs/busy.gif -o openjscad/imgs/busy.gif
+dl() {
+  URL=https://raw.githubusercontent.com/Spiritdude/OpenJSCAD.org/master/$1
+  FILE=openjscad/$2
 
-curl https://raw.githubusercontent.com/Spiritdude/OpenJSCAD.org/master/formats.js -o openjscad/formats.js
+  if [ -n "$OVERWRITE" ] ; then
+    echo "rm $FILE"
+    [ -f $FILE ] && rm -f $FILE
+    # [ -f $FILE ] && echo "no $FILE"
+  else
+    echo "update if newer $FILE"
+    DASHZ="-z $FILE"
+  fi
+
+  curl -Ss $URL $DASHZ -o $FILE && {
+    echo "$1 ok"
+  } || {
+    echo "$1 error$?"
+  }
+}
+
+dl openjscad.js openjscad.js
+dl openscad.js openscad.js
+dl js/jscad-function.js jscad-function.js
+dl js/jscad-worker.js jscad-worker.js
+dl lightgl.js lightgl.js
+dl csg.js csg.js
+dl Blob.js Blob.js
+dl imgs/busy.gif imgs/busy.gif
+dl formats.js formats.js
+
 patch openjscad/formats.js formats.patch
